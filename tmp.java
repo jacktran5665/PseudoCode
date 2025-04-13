75f43c9c853f408d7a16b5a38498a4908c38e47c
Jack Tran <jacktran14122004@gmail.com>
2025-04-13 12:48:00 -0400

    Create tmp.java

---
+++ tmp.java
@@ -0,0 +1,17 @@
+// local triviaquestion = require.(game.replicatedstorage.triviaquestion)
+
+//method copyData()
+// local copy = {}
+//  create a for loop to pass the data over
+// print(copy)
+// return copy
+
+
+//method get()
+// get randomQuestion
+// remove that question
+// get question then display
+// get option then display
+// get answer store 
+// store question+answer in a variable
+
