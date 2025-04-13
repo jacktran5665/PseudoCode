516811be5c4d5a2f045ef86171e8b7add86c78a7
Jack Tran <jacktran14122004@gmail.com>
2025-04-13 13:05:44 -0400

    Create gunner.java

---
+++ gunner.java
@@ -0,0 +1,16 @@
+// method intiGun()
+// chamber = [,,,,]
+// # of bullet
+// randomize(chamber[# of bullet])
+//
+
+
+
+
+//method gun()
+// check the current chamber
+// if chamber = bullet
+// return fire
+// else
+// increment chamber by 1
+
