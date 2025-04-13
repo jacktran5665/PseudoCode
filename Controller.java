6270f2031457fbb08f072ea2e9cb25a90366707f
Jack Tran <jacktran14122004@gmail.com>
2025-04-13 12:39:36 -0400

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,26 @@
+// method decider()
+
+
+// if (player.choice != answer) {
+//     gun()
+// }
+// switch()
+
+
+
+
+// if player == initial # of player 
+// intialize all script
+//
+
+// method tmp()
+// 
+
+
+
+// method getdata()
+// fetch data from question storage script(question, options, answer)
+// local getdata = require(game.replicatestorage.triviaquestion) the location of the sctorage script
+// create a for loop to copy the data over to a temporary storage script separate from the controller
+
+// 
